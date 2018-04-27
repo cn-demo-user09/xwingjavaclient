665e40abab76670e3d69d4802b21319be0473b52
cn-demo-user09 <34110068+cn-demo-user09@users.noreply.github.com>
2018-04-27 15:51:20 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -40,7 +40,7 @@ public class App {
  
         System.out.println("Server started");
  
-        //runGetRequest();
+        runGetRequest();
         //runDatabaseQuery();
     }
  
@@ -50,7 +50,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://ip.jsontest.com/";
+        String url = "http://129.144.148.225:3000/shield/33/45/blue/javafighteruser01";
         CloseableHttpResponse response = null;
  
         try {
